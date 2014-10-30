7fc9e23adbeefd4e331bb2997147b3d94c7b8ffd
Peter Wang <happypeter1983@gmail.com>
2014-10-30 03:28:48 +0000

    pusher-gem works here

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def hello
-    Pusher.trigger('peter_channel', 'peter_event', {message: 'heee'})
+    Pusher.trigger_async('peter_channel', 'peter_event', {message: 'heee'})
   end
 end
